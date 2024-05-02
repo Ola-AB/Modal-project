cd130ff0bfc4f9a65b7890dcd0a2781f2b0a84c8
Ola <olo2040@wp.pl>
2024-05-02 17:59:55 +0200

    added function of close modal by escape key

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 	btnsOpenModal[i].addEventListener("click", openModal);
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+document.addEventListener('keyup', function (e) {
+console.log(e.key);
+
+if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+		closeModal();
+	}
+});
